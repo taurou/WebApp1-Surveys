02fc0c9b6a5075ed1daadcb7b6aa30b6cd7d9fa0
s281605 <s281605@studenti.polito.it>
2021-06-17 15:26:30 +0200

    added login

---
+++ server/user-dao.js
@@ -0,0 +1,41 @@
+'use strict';
+const db = require('./db');
+const bcrypt = require('bcrypt');
+
+// DAO operations for validating users
+exports.getUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM user WHERE email = ?';
+        db.get(sql, [email], (err, row) => {
+            if (err)
+                reject(err); // DB error
+            else if (row === undefined)
+                resolve(false); // user not found
+            else {
+              bcrypt.compare(password, row.hash).then(result => {
+                    if (result) // password matches
+                        resolve({id: row.id, email: row.email, name: row.name});
+                    else
+                        resolve(false); // password not matching
+                })
+            }
+        });
+    });
+};
+
+exports.getUserById = (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM user WHERE id = ?';
+        db.get(sql, [id], (err, row) => {
+          if (err) 
+            reject(err);
+          else if (row === undefined)
+            resolve({error: 'User not found.'});
+          else {
+            const user = {id: row.id, email: row.email }
+            resolve(user);
+          }
+      });
+    });
+  };
+  
