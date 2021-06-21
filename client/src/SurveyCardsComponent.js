6a217c2f2b5ae7f0d1e2d0069b5e3e47db184c3a
s281605 <s281605@studenti.polito.it>
2021-06-22 00:27:03 +0200

    quasi-final 2

--- client/src/SurveyCardsComponent.js
+++ client/src/SurveyCardsComponent.js
@@ -12,10 +12,17 @@ function SurveyCards(props) {
           <Card.Body>
                         <Card.Title>{item.Questions.nameSurvey}</Card.Title>
 {/* TODO sistemare aspetto delnumero di risposte e vedere se si può usare filter anziché map */}
+
+
+
                         { props.isAdmin ? props.countAnswers.map((obj,k ) => ( obj.SurveyId === item.SurveyId ? <><Button key={item.Questions.nameSurvey+"_1"+k} style={{margin: '3px'}}disabled>{obj.NumRisposte}</Button><Link key={item.Questions.nameSurvey+"_4"+k}  to={`viewanswers/${item.SurveyId}/0`} >
-            <Button key={item.Questions.nameSurvey+"_2"} variant="success">Check answers</Button></Link></>  : "")) : <Link key={item.Questions.nameSurvey+"_7"}  to={`answersurvey/${item.SurveyId}`} >
+            <Button key={item.Questions.nameSurvey+"_2"} variant="success">Check answers</Button></Link></>  :  props.countAnswers.some(el => el.SurveyId === item.SurveyId) ? "" : <><Button key={item.Questions.nameSurvey+"_11"+k} style={{margin: '3px'}} disabled>0</Button>
+            <Button key={item.Questions.nameSurvey+"_12"} disabled variant="secondary">No answers yet</Button></>  )) : <Link key={item.Questions.nameSurvey+"_7"}  to={`answersurvey/${item.SurveyId}`} >
             <Button key={item.Questions.nameSurvey+"_3"} variant="success">Answer this survey</Button></Link> }
           </Card.Body>
+            {/* interessante questo .some()
+             che restituisce se all'interno di un vettore di oggetti 
+             è contenuto un elemento con determinate caratteristiche (in questo caso con un determinato SurveyId) */}
 
         </Card>
       )
@@ -28,3 +35,6 @@ function SurveyCards(props) {
   
 }
 export default SurveyCards
+
+
+
